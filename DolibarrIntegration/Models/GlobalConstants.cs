9c850ce54c30f5caf9ce931adc3c0a54377f7b32
Niels De Los Santos <kilohdnartis@gmail.com>
2022-08-11 20:13:33 -0400

    Para registrar las constantes.

---
+++ DolibarrIntegration/Models/GlobalConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DolibarrIntegration.Models
+{
+    public class GlobalConstants
+    {
+        public string Url = "http://localhost:85/dolibarr/api/index.php/";
+    }
+}
