ed5b3132315650fba0004d4f94fb97edcbe02630
Nancy Campusano <nancycampusano1@gmail.com>
2022-08-13 05:41:39 +0000

    Adding Products Create

--- DolibarrIntegration/Models/GlobalConstants.cs
+++ DolibarrIntegration/Models/GlobalConstants.cs
@@ -2,9 +2,11 @@
 {
     public static class GlobalConstants
     {
-        public static string Url = "http://localhost:85/dolibarr/api/index.php/";
+        public static string Url = "http://localhost:80/dolibarr/api/index.php/";
 
         public static string GetContacts = "http://localhost:80/dolibarr/api/index.php/contacts?";
+        public static string GetProducts = "http://localhost:80/dolibarr/api/index.php/products?";
+
 
         public static string Token = "123qwe";
     }
---
+++ DolibarrIntegration/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace DolibarrIntegration.Models
+{
+    
+    public class Product
+    {
+        [Display(Name = "Referencia")]
+        public string @ref { get; set; }
+
+        [Display(Name = "Producto")]
+        public string label { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string description { get; set; }
+
+        [Display(Name = "Precio")]
+        public decimal buyprice { get; set; }
+    }
+}
